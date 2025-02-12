7df070cffd317471b48cb89f050f7450ca0d478c
Winston Neo <winstonseanqui@gmail.com>
2025-02-12 16:21:49 +0800

    Startup Card Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { id: 1, name: "John Doe", image: "https://placehold.co/48x48" },
+      _id: 1,
+      description: "A new way to connect with friends",
+      image: "https://picsum.photos/seed/picsum/200/200",
+      category: "Social Media",
+      title: "Friend Connect",
+    },
+  ];
 
   return (
     <>
@@ -19,6 +33,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest Pitches"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No results found.</p>
+          )}
+        </ul>
       </section>
     </>
   );

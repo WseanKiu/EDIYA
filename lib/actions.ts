ad1e7ec443556ecd9d8fbcae5e04178fca168ef1
Winston Neo <winstonseanqui@gmail.com>
2025-02-19 18:42:42 +0800

    Create content form with Server action and toast

---
+++ lib/actions.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { auth } from "@/auth";
+import { parseServerActionResponse } from "./utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const createIdea = async (state: any, form: FormData, pitch: string) => {
+  const session = await auth();
+
+  if (!session)
+    return parseServerActionResponse({
+      error: "Not authenticated",
+      status: "ERROR",
+    });
+
+  const { title, description, category, link } = Object.fromEntries(
+    Array.from(form).filter(([key]) => key !== "pitch")
+  );
+
+  const slug = slugify(title as string, { lower: true, strict: true });
+
+  try {
+    const startup = {
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: slug,
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session?.id,
+      },
+      pitch,
+    };
+
+    const result = await writeClient.create({ _type: "startup", ...startup });
+
+    return parseServerActionResponse({
+      ...result,
+      status: "SUCCESS",
+      error: "",
+    });
+  } catch (error) {
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
+};

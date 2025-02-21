3057144c25f8945a2d3755d740db7996c510007c
Winston Neo <winstonseanqui@gmail.com>
2025-02-22 00:19:08 +0800

    fix session id and primary color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-import { auth, signOut } from "@/auth";
-import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { auth, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
@@ -11,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src={"/logo.png"} alt="Logo" width={144} height={30} />
+          <Image src={"/logo.png"} alt="Logo" width={120} height={40} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
@@ -34,13 +34,13 @@ const Navbar = async () => {
                 </button>
               </form>
 
-              <Link href={`/user/${session?.user?.id}`}>
+              <Link href={`/user/${session?.id}`}>
                 <Avatar className="size-10">
                   <AvatarImage
                     src={session?.user?.image}
                     alt={session?.user?.name}
                   />
-                  <AvatarFallback>AV</AvatarFallback>
+                  <AvatarFallback>{session?.user?.name[0]}</AvatarFallback>
                 </Avatar>
               </Link>
             </>

35b7b0c9d5616a1c1412c1819637a082360959b5
Winston Neo <winstonseanqui@gmail.com>
2025-02-22 01:39:46 +0800

    fix profile session id and build ts/eslint error

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
           <p className="text-30-bold">
-            {session.id === id ? "Your" : `${user.name}'s`} Pitches
+            {session?.id === id ? "Your" : `${user.name}'s`} Pitches
           </p>
           <ul className="card_grid-sm">
             <Suspense fallback={<StartupCardSkeleton />}>
